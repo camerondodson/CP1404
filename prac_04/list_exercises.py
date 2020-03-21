ef1f357c215b38eeb01454ab1ba4866e25d40ae5
Cameron <cameron.dodson@my.jcu.edu.au>
2020-03-21 13:44:25 +1000

    Create list_exercises.py and each print line

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,24 @@
+numbers = []
+
+# Instructions for user
+print("Please enter 5 numbers")
+
+# Get user to input each of the 5 numbers
+for number in range(1, 6):
+    number = int(input("Enter number {}: ".format(number)))
+    numbers.append(number)
+
+# Find the first number
+print("The first number is {}".format(numbers[0]))
+
+# Find the last number
+print("The last number is {}".format(numbers[-1]))
+
+# Find the smallest number
+print("The smallest number is {}".format(min(numbers)))
+
+# Find the largest number
+print("The largest number is {}".format(max(numbers)))
+
+# Find the average of all 5 numbers
+print("The average of the numbers is {}".format(sum(numbers)/5))
