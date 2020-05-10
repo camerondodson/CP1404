6b23038d386e1a59b8467106ecefd705c20651b0
Cameron <cameron.dodson@my.jcu.edu.au>
2020-05-10 19:43:22 +1000

    Finish walkthrough example

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,9 @@
+from prac_08.taxi import Taxi
+
+
+taxi_1 = Taxi("Prius 1", 100)
+taxi_1.drive(40)
+print("{}, ${}".format(taxi_1, taxi_1.get_fare()))
+taxi_1.start_fare()
+taxi_1.drive(100)
+print("{}, ${}".format(taxi_1, taxi_1.get_fare()))
